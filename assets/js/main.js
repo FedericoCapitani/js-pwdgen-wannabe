8ac62cd327960b0f23199ff684f229398932967c
FedericoCapitani <federico.capitani@outlook.it>
2022-03-23 14:39:27 +0100

    completare esercizio pwdgen

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,16 @@
 
 // chiedere nome all'utente
 
+const first_name = prompt('Inserisci il tuo nome');
+console.log(first_name);
+
 // chiedere cognome all'utente
+const last_name = prompt('Inserisci il tuo cognome');
+console.log(last_name);
 
 // chiedere colore preferito all'utente
+const color = prompt('Inserisci il tuo colore preferito');
+console.log(color);
 
 // scrivere la pwd generta
+document.getElementById('pwd').innerHTML = `${first_name}${last_name}${color}22`
